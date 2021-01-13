af2404436f591c24be379a4533933e069db62bcc
gadgetvala <surajj.verma7@gmail.com>
2021-01-13 16:28:00 +0530

    Added Data Model

---
+++ food/lib/models/data.dart
@@ -0,0 +1,117 @@
+class Data {
+  static List menuData = [
+    {
+      'itemName': 'Burger',
+      'imgLink': 'assets/burger-menu.png',
+      'itemCode': 'z1',
+    },
+    {
+      'itemName': 'Donout',
+      'imgLink': 'assets/donout-menu.png',
+      'itemCode': 'z2',
+    },
+    {
+      'itemName': 'Ice-Cream',
+      'imgLink': 'assets/icecream-menu.png',
+      'itemCode': 'z3'
+    },
+    {
+      'itemName': 'Sandwich',
+      'imgLink': 'assets/sandwich-menu.png',
+      'itemCode': 'z4'
+    },
+    {
+      'itemName': 'Burger',
+      'imgLink': 'assets/burger-menu.png',
+      'itemCode': 'z5',
+    },
+    {
+      'itemName': 'Donout',
+      'imgLink': 'assets/donout-menu.png',
+      'itemCode': 'z6',
+    },
+  ];
+
+  static List products = [
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z11',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z',
+      'productCode': 'z12',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z13',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z14',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z15',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z16',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z17',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z18',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z19',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+    {
+      'categoryCode': 'z1',
+      'productCode': 'z110',
+      'productName': "Burger",
+      'previewPrice': "10.0",
+      'newPrive': '8.3',
+      'imgLink': ""
+    },
+  ];
+}
