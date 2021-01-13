72a7fc9d587fd502d8378b9c3273ee4d92e4d894
gadgetvala <surajj.verma7@gmail.com>
2021-01-13 16:28:13 +0530

    Added New Widgets

--- food/lib/views/homepage_screen.dart
+++ food/lib/views/homepage_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
 import './../widgets/homepage/homepageHeader.dart';
+import './../widgets/homepage/headerSelectionMenu.dart';
+import './../widgets/homepage/productMainHeaders.dart';
+import './../widgets/homepage/productPage.dart';
 
-class HomepageScreen extends StatelessWidget {
+class HomepageScreen extends StatefulWidget {
   static String routeName = '/homepage';
+
+  @override
+  _HomepageScreenState createState() => _HomepageScreenState();
+}
+
+class _HomepageScreenState extends State<HomepageScreen> {
+  String currentSelectedItem = 'z1';
+
+  setSelectedItem(itemCode) {
+    setState(() {
+      currentSelectedItem = itemCode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +29,11 @@ class HomepageScreen extends StatelessWidget {
         child: Column(
           children: <Widget>[
             HomepageHeader(),
+            SizedBox(height: 10),
+            HeaderSelectionMenu(setSelectedItem, currentSelectedItem),
+            SizedBox(height: 30),
+            ProductMainHeaders(),
+            ProductPage(currentSelectedItem),
           ],
         ),
       ),
