2b986a7a7c7833d02ded6513fb2ee5bf8789a360
gadgetvala <surajj.verma7@gmail.com>
2021-03-14 17:15:30 +0530

    Added Models

---
+++ movie_database/lib/models/movies.dart
@@ -0,0 +1,32 @@
+final movies = [
+  {
+    "key": "A",
+    "image": "https://placeimg.com/640/480/any",
+    "name": "Something 1",
+    "rating": "8.2"
+  },
+  {
+    "key": "B",
+    "image": "https://placeimg.com/640/480/nature",
+    "name": "Something 2",
+    "rating": "4.2"
+  },
+  {
+    "key": "C",
+    "image": "https://placeimg.com/640/480/prople",
+    "name": "Something 3",
+    "rating": "8.5"
+  },
+  {
+    "key": "D",
+    "image": "https://placeimg.com/640/480/nature",
+    "name": "Something 4",
+    "rating": "9.5"
+  },
+  {
+    "key": "E",
+    "image": "https://placeimg.com/640/480/nature",
+    "name": "Something 5",
+    "rating": "6.5"
+  },
+];
