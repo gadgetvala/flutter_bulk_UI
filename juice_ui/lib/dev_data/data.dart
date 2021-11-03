5fdaf2d00e00c064beb5c0eb6d4d98658683d85f
gadgetvala <surajj.verma7@gmail.com>
2021-11-03 15:35:10 +0530

    FEATURE: Added More Dummy Data

--- juice_ui/lib/dev_data/data.dart
+++ juice_ui/lib/dev_data/data.dart
@@ -1,28 +1,76 @@
 import 'package:juice/res/res.dart';
 
-final List<Map<String, dynamic>> data = [
-  {
-    "id": 1,
-    "name": "Plum",
-    "price": 5,
-    "image": assets.plumDrink,
-  },
-  {
-    "id": 2,
-    "name": "Celery",
-    "price": 3,
-    "image": assets.celeryDrink,
-  },
-  {
-    "id": 3,
-    "name": "Blueberry",
-    "price": 4,
-    "image": assets.blueBerryDrink,
-  },
-  {
-    "id": 4,
-    "name": "Orange",
-    "price": 7,
-    "image": assets.orangeDrink,
-  },
+final List<List<Map<String, dynamic>>> data = [
+  [
+    {
+      "id": 1,
+      "name": "Plum",
+      "price": 5,
+      "image": assets.plumDrink,
+      "color": colors.plumColor,
+      "color_dark": colors.plumColorDark,
+    },
+    {
+      "id": 2,
+      " name": "Celery",
+      "price": 3,
+      "image": assets.celeryDrink,
+      "color": colors.celeryColor,
+      "color_dark": colors.celeryColorDark,
+    },
+  ],
+  [
+    {
+      "id": 3,
+      "name": "Blueberry",
+      "price": 4,
+      "image": assets.blueBerryDrink,
+      "color": colors.blueberryColor,
+      "color_dark": colors.blueberryColorDark,
+    },
+    {
+      "id": 4,
+      "name": "Orange",
+      "price": 7,
+      "image": assets.orangeDrink,
+      "color": colors.orangeColor,
+      "color_dark": colors.orangeColorDark,
+    },
+  ],
+  [
+    {
+      "id": 5,
+      " name": "Celery",
+      "price": 3,
+      "image": assets.celeryDrink,
+      "color": colors.celeryColor,
+      "color_dark": colors.celeryColorDark,
+    },
+    {
+      "id": 6,
+      "name": "Plum",
+      "price": 5,
+      "image": assets.plumDrink,
+      "color": colors.plumColor,
+      "color_dark": colors.plumColorDark,
+    },
+  ],
+  [
+    {
+      "id": 7,
+      "name": "Blueberry",
+      "price": 4,
+      "image": assets.blueBerryDrink,
+      "color": colors.blueberryColor,
+      "color_dark": colors.blueberryColorDark,
+    },
+    {
+      "id": 8,
+      "name": "Orange",
+      "price": 7,
+      "image": assets.orangeDrink,
+      "color": colors.orangeColor,
+      "color_dark": colors.orangeColorDark,
+    },
+  ]
 ];
